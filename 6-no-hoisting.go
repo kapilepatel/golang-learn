6f2cc46383b7ec12cf64961a1fbaaf2eda29fec1
Kapil Patel <kapilepatel@gmail.com>
2024-10-05 14:31:35 +0530

    update comment in no hoisting

--- 6-no-hoisting.go
+++ 6-no-hoisting.go
@@ -5,7 +5,8 @@ import (
 )
 
 func main() {
-	//below will fail with undefinedv
+	//below will fail with undefined
+	//no hoisting- variable has to be declared before use, it follows as top down
 	fmt.Println(num1)
 	var num1 int = 1
 
