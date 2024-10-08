5783b64310e286b23103b573bb8eaacbe6c927e9
Kapil Patel <kapilepatel@gmail.com>
2024-10-08 12:57:29 +0530

    Updated folder structure

--- 6-no-hoisting/6-no-hoisting.go
+++ 6-no-hoisting/6-no-hoisting.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	//below will fail with undefined
 	//no hoisting- variable has to be declared before use, it follows as top down
-	fmt.Println(num1)
-	var num1 int = 1
+	//fmt.Println(num1)
+	//var num1 int = 1
 
 }
