9c6ff03093093e625cf6038f42cbeaf3dfcde001
Kapil Patel <kapilepatel@gmail.com>
2024-10-10 01:58:53 +0530

    map key value range

---
+++ 16-map/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//creating a data structure called Map
+	//Funny here missing the last comma creates compile error
+	//so we need to put comma or closing curly braces in same line
+	//key has to be unique
+	m := map[string]int{
+		"item1":  10,
+		"itemx":  12,
+		"itemzz": -65,
+		"":       0,
+		"a b":    0,
+	}
+	for k, v := range m {
+		fmt.Printf("Key: %v - Value: %v \n", k, v)
+	}
+}
